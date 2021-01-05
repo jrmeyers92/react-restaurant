bc9a177faf04445792689fb68f0aa668ef9ba78c
Jake Meyers <jakejake92@yahoo.com>
2021-01-05 15:40:37 -0700

    pictures added to homepage, title fixed

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,13 +2,19 @@ import React from "react";
 import "./Home.css";
 import Header from "../Shared/Header/Header";
 import Footer from "../Shared/Footer/Footer";
+import Coffee from "../Assets/coffee.png";
 
 const Home = () => {
 	return (
 		<div className='home'>
 			<Header />
 			<div className='home__pictures'>
-				<img src='coffee.png'></img>
+				<img
+					className='home__image bloodyMary'
+					src='https://doubleshot.cafe/wp-content/themes/doubleshot/graphics/home-tiles/bloody_mary.png'></img>
+				<img
+					className='home__image coffee'
+					src='https://doubleshot.cafe/wp-content/themes/doubleshot/graphics/home-tiles/coffee.png'></img>
 			</div>
 			<Footer />
 		</div>
