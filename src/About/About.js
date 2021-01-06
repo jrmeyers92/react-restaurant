1e8dfb71142f895ee5c9dcc15ca9cb088b0c644a
Jake Meyers <jakejake92@yahoo.com>
2021-01-05 21:18:06 -0700

    about page done

---
+++ src/About/About.js
@@ -0,0 +1,72 @@
+import React from "react";
+import "./About.css";
+import Header from "../Shared/Header/Header";
+import Menu from "../Shared/Menu/Menu";
+import Footer from "../Shared/Footer/Footer";
+
+const About = () => {
+	return (
+		<div className='about'>
+			<Header />
+			<div className='about__content'>
+				<div className='column'>
+					<h2 className='about__header'>The Cafe</h2>
+					<div className='about__blurb'>
+						<h3 className='about__blurb-header'>The Life</h3>
+						<p className='about__blurb-para'>
+							Nestled neatly in the local Deakin shops is a social hub,
+							pulsating vibrancy and life. Friendly and fun, Doubleshot indulges
+							Canberra’s inner south with it’s quirky interiors and love-filled
+							menu, serving breakfast through to lunch. Doubleshot is your
+							perfect hangout for a carefree coffee catch-up, a date with your
+							fellow breakfast lover or a social afternoon running into friends.
+						</p>
+					</div>
+
+					<div className='about__blurb'>
+						<h3 className='about__blurb-header'>The Food</h3>
+						<p className='about__blurb-para'>
+							Using fresh produce, innovative thinking and a customer-oriented
+							approach, you’ll feel right at home as you sit and sip on their
+							famous peanut butter smoothie. Become a part of the relaxed buzz
+							that is the Doubleshot culture – atmospheric, creative and homely
+							every day.
+						</p>
+					</div>
+				</div>
+
+				<div className='column'>
+					<h2 className='about__header'>The Story</h2>
+					<div className='about__blurb'>
+						<h3 className='about__blurb-header'>The Atmosphere</h3>
+						<p className='about__blurb-para'>
+							Complete with a hip, creative homely atmosphere, its your local
+							fix of fresh produce, innovative tailor-made menus and
+							customer-orientated service. Let one of our finely roasted coffees
+							grace your lips before the days dance begins. With large indoor
+							and out door designer dining areas, Doubleshot lets you snuggle in
+							or freshen up under the suns warmth, depending on your mood.
+						</p>
+					</div>
+
+					<div className='about__blurb'>
+						<h3 className='about__blurb-header'>And so it began...</h3>
+						<p className='about__blurb-para'>
+							The words ‘I wake up wanting to kiss you’ are scrawled across the
+							wall, reminding us of that first cup of coffee, kissing our lips
+							in the morning and inviting us to have a good day. The Doubleshot
+							story continues to unfold with innovative ideas, changing menus
+							and people writing their own stories between the walls as they
+							indulge in daily delights.
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<Menu />
+			<Footer />
+		</div>
+	);
+};
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import Home from "./Home/Home";
+import About from "./About/About";
 
 function App() {
 	return (
 		<div className='App'>
-			<Home />
+			<About />
 		</div>
 	);
 }
